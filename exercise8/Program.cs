770bb93737e8c8044013ce5f10cf9d8faa530293
KeanuParker0402 <keanu.parker@s.capregboces.org>
2024-03-04 13:17:32 -0500

    exe8

---
+++ exercise8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter a max number:");
+            int maxNumb = int.Parse (Console.ReadLine());
+
+            for (int i = 0; i < maxNumb; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine(maxNumb);
+        }
+    }
+}
